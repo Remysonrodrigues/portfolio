c1eb08f0b818d9569770174b0a025ab260350c37
Remyson Rodrigues <remysonrodrigues@gmail.com>
2020-12-14 10:32:37 -0300

    Add api-portfolio

--- portfolio-api/app.js
+++ portfolio-api/app.js
@@ -29,7 +29,7 @@ app.post('/api/email', (req, res, next) => {
 
     console.log(req.body);
 
-    sendGrid.setApiKey('chave-da-api');
+    sendGrid.setApiKey('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');
     const msg = {
         to: 'remysonrodrigues@gmail.com',
         from: req.body.email,
